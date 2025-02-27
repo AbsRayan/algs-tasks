45b7f614a801aa0f6a5046e76355c39daa18739c
RayanAbs <rayanabsalyamov0610@gmail.com>
2025-02-27 12:43:32 +0300

    task1 contest1

--- task1.py
+++ task1.py
@@ -1,1 +1,25 @@
-print("Hello")
+def binary_search(a, elem_b, n):
+    if elem_b <= a[0]:
+        return 0
+    
+    high = 1 
+    while (high < n) and (elem_b > a[high]) :
+        high *= 2
+
+    high = min(high, n)
+    low = high // 2
+
+    while low < high:
+        middle = (high + low)//2
+        if elem_b == a[middle]:
+            return middle
+        if elem_b < a[middle]:
+            high = middle
+        else: 
+            low = middle + 1
+    return low
+
+
+                
+
+
